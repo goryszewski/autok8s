a2edc524f54b04595ed813722a10885c4c860c64
Michal Goryszewski <goryszewski@gmail.com>
2023-12-16 23:41:43 +0100

    Fix proxy, Add domian for k8s

--- terraform/modules/domain/files/prep.sh
+++ terraform/modules/domain/files/prep.sh
@@ -10,7 +10,9 @@ update-ca-certificates
 
 # DOTO add proxy
 
-#  cat <<EOF | sudo tee /etc/profile.d/proxy.sh
-# export http_proxy="http://192.168.100.10:3128"
-# export https_proxy="http://192.168.100.10:3128"
-# EOF
+ cat <<EOF | sudo tee /etc/environment
+http_proxy="http://192.168.100.10:3128"
+https_proxy="http://192.168.100.10:3128"
+no_proxy="localhost,127.0.0.1,10.0.0.0/8"
+NO_PROXY="localhost,127.0.0.1,10.0.0.0/8"
+EOF
